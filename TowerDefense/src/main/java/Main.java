c54c64e858c5cc73068338e60ec162d1ff98fb2f
RHoffmann21 <68516669+RHoffmann21@users.noreply.github.com>
2020-07-19 22:27:30 +0200

    Update Main.java

--- TowerDefense/src/main/java/Main.java
+++ TowerDefense/src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
 // na du :P
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("so ich hoffe das reicht als Beispiel");
 	}
 
 }
